fb24b7d57d5452027493a9dde091821b97142302
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2025-11-15 14:40:13 +0300

    Create syntax.h

---
+++ syntax.h
@@ -0,0 +1,36 @@
+#include <a.out.h>
+#include <adcintrin.h>
+#include <adxintrin.h>
+#include <aio.h>
+#include <stdio.h>
+#include <stdbool.h>
+#ifndef CODE
+#define CODE
+#define null NULL
+#define print printf
+#define str char
+#define str8 char8_t
+#define str16 char16_t
+#define str32 char32_t
+#define function void
+#define null_return return(0)
+#define int8 __int8_t
+#define int16 __int16_t
+#define int32 __int32_t
+#define int64 __int64_t
+#define int_least8 __int_least8_t
+#define int_least16 __int_least16_t
+#define int_least32 __int_least32_t
+#define int_least64 __int_least64_t
+#define U8 __U8_TYPE
+#define U16 __U16_TYPE
+#define U32 __U32_TYPE
+#define U64 __U64_TYPE
+#define uint8 __uint8_t
+#define uint16 __uint16_t
+#define uint32 __uint32_t
+#define uint64 __uint64_t
+#define uint128 __uint128_t
+#define ino __ino_t
+#define ino64 __ino64_t
+#endif
